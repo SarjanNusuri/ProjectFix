3c66a8a7719eceaa82ec08db285d6a49883045ad
adhitD <rahmatferdinan7@gmail.com>
2025-10-15 15:13:16 +0800

    perubahan produk

--- database/migrations/2025_10_15_071103_create_product_table.php
+++ database/migrations/2025_10_15_071103_create_product_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('product', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->integer('stock');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
